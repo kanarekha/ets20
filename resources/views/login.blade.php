6a8aaeb305e28a409db50f51ba21679ba9d301b5
kanarekha <kanarekha8@gmail.com>
2020-10-29 12:37:11 +0700

    ubah design login dan regist

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -27,14 +27,13 @@
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
-      <div class="col-xl-10 col-lg-12 col-md-9">
+      <div class="col-lg-6">
 
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
+              <div class="col-lg">
                 <div class="p-5">
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Login</h1>
@@ -47,13 +46,13 @@
                   <form class="user" action="/login/dashboard">
                   
                     <div class="form-group">
-                      <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter Email Address..." value="{{ old('email') }}">
+                      <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter Email" value="{{ old('email') }}">
                       @error('email')
                         <div class="invalid-feedback">{{ $message }} </div>
                       @enderror
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" placeholder="Password">
+                      <input type="password" class="form-control form-control-user @error('password') is-invalid @enderror" id="password" name="password" placeholder="Enter Password">
                       @error('password')
                         <div class="invalid-feedback">{{ $message }} </div>
                       @enderror
@@ -65,7 +64,7 @@
                       </div>
                     </div>
                     <button type="submit" class="btn btn-primary btn-user btn-block">
-                      Login test
+                      Login 
                     </button>
                     <hr>
                   </form>
--- resources/views/registrasi.blade.php
+++ resources/views/registrasi.blade.php
@@ -24,12 +24,11 @@
 
   <div class="container">
 
-    <div class="card o-hidden border-0 shadow-lg my-5">
+    <div class="card o-hidden border-0 shadow-lg my-5 col-lg-6 mx-auto">
       <div class="card-body p-0">
         <!-- Nested Row within Card Body -->
         <div class="row">
-          <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-          <div class="col-lg-7">
+          <div class="col-lg">
             <div class="p-5">
               <div class="text-center">
                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
@@ -43,39 +42,39 @@
               @csrf
                 <div class="form-group row">
                   <div class="col-sm-12 mb-3 mb-sm-0">
-                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Name" name="name" value="{{ old('name') }}">
+                    <input type="text" class="form-control form-control-user @error('name') is-invalid @enderror" id="name" placeholder="Name" name="name" value="{{ old('name') }}">
                     @error('name')
                       <div class="invalid-feedback">{{ $message }} </div>
                     @enderror
                   </div>
                 </div>
                 <div class="form-group">
-                  <input type="email"  class="form-control @error('email') is-invalid @enderror" id="exampleInputEmail" placeholder="Email Address" name="email" value="{{ old('email') }}">
+                  <input type="email"  class="form-control form-control-user @error('email') is-invalid @enderror" id="exampleInputEmail" placeholder="Email Address" name="email" value="{{ old('email') }}">
                   @error('email')
                     <div class="invalid-feedback">{{ $message }} </div>
                   @enderror
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="password"  class="form-control @error('password') is-invalid @enderror" id="exampleInputPassword" placeholder="Password" name="password">
+                    <input type="password"  class="form-control form-control-user @error('password') is-invalid @enderror" id="exampleInputPassword" placeholder="Password" name="password">
                     @error('password')
                       <div class="invalid-feedback">{{ $message }} </div>
                     @enderror
                   </div>
                   <div class="col-sm-6">
-                    <input type="password"  class="form-control @error('password_confirmation') is-invalid @enderror" id="exampleRepeatPassword" placeholder="Repeat Password" name="password_confirmation">
+                    <input type="password"  class="form-control form-control-user @error('password_confirmation') is-invalid @enderror" id="exampleRepeatPassword" placeholder="Repeat Password" name="password_confirmation">
                     @error('password_confirmation')
                       <div class="invalid-feedback">{{ $message }} </div>
                     @enderror
                   </div>
                 </div>
                 <div class="form-group">
-                  <input type="text"  class="form-control @error('phone') is-invalid @enderror" id="phone" placeholder="Phone Number" name="phone" value="{{ old('phone') }}">
+                  <input type="text"  class="form-control form-control-user @error('phone') is-invalid @enderror" id="phone" placeholder="Phone Number" name="phone" value="{{ old('phone') }}">
                   @error('phone')
                     <div class="invalid-feedback">{{ $message }} </div>
                   @enderror
                 </div>
-                <button type="submit" class="btn btn-primary">Create</button>
+                <button type="submit" class="btn btn-primary btn-user btn-block ">Create</button>
                 <hr>
               </form>
               <div class="text-center">
