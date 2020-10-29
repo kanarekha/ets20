a5f066e1dab716d9b8a2def6a21792b571f0e936
kanarekha <kanarekha8@gmail.com>
2020-10-29 11:34:20 +0700

    commit first kana

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('dashboard');
+});
+
+Route::get('/register', 'RegistrasiController@index');
+
+Route::get('/login', 'LoginController@index');
+Route::get('/login/dashboard', 'LoginController@index1');
