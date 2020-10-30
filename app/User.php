c8b9beccc95818ff457e37c59575e92f163b14cf
ridaadila <ridaadila10@gmail.com>
2020-10-30 11:25:31 +0700

    bisa login dan create user

--- app/User.php
+++ app/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
     use Notifiable;
 
     public $incrementing = false;
-    protected $table = 'users';
+    protected $table = 'user';
 
     /**
      * The attributes that are mass assignable.
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name_user', 'email', 'password','phone_number'
     ];
 
     /**
